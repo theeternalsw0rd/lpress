ee8528bc1bf595e929601825e27e90d7e838df10
Micah Bucy <micah.bucy@eternalsword.com>
2013-05-02 18:00:46 -0400

    initial

---
+++ src/EternalSword/LPress/LPressServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace EternalSword\LPress;
+
+use Illuminate\Support\ServiceProvider;
+
+class LPressServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('eternal-sword/l-press');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
