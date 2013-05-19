4bec4c9f2538236e3d0cb335ec3224bf37699b5b
Micah Bucy <micah.bucy@eternalsword.com>
2013-05-18 21:25:47 -0400

    bring blade templates up to par

--- src/views/themes/default/logged-in.blade.php
+++ src/views/themes/default/logged-in.blade.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Already Logged In</title>
-	</head>
-	<body>
-		<h1>You are currently logged in as {{ Auth::user()->username }}</h1>
-		<p>If this is not your account, please click {{ Link::link_to_route('lpress-logout-login', 'here') }} and try again.</p>
-	</body>
-</html>
+@extends($view_prefix . '.layouts.master')
+@section('content')
+	<h1>You are currently logged in as {{ Auth::user()->username }}</h1>
+	<p>If this is not your account, please click {{ Link::link_to_route('lpress-logout-login', 'here') }} and try again.</p>
+@stop
--- src/views/themes/default/login.blade.php
+++ src/views/themes/default/login.blade.php
@@ -1,34 +1,29 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Hello World</title>
-	</head>
-	<body>
-		<h1>Login</h1>
-		@if($login_failed)
-			<div class='error'>
-				<p>Login failed, please try again.</p>
-			</div>
-		@endif
-		<div class='form'>
-			{{ Form::open() }}
-			<div class='text'>
-				{{ Form::label('Username') }}<br />
-				{{ Form::text('username') }}
-			</div>
-			<div class='text'>
-				{{ Form::label('Password') }}<br />
-				{{ Form::text('password') }}
-			</div>
-			<div class='checkbox'>
-				{{ Form::label('Remember me') }}
-				{{ Form::checkbox('remember') }}
-			</div>
-			{{ Form::token() }}
-			<div class='input-button'>
-				{{ Form::submit('Submit') }}
-			</div>
-			{{ Form::close() }}
+@extends($view_prefix . '.layouts.master')
+@section('content')
+	<h1>Login</h1>
+	@if($login_failed)
+		<div class='error'>
+			<p>Login failed, please try again.</p>
 		</div>
-	</body>
-</html>
+	@endif
+	<div class='form'>
+		{{ Form::open() }}
+		<div class='text'>
+			{{ Form::label('Username') }}<br />
+			{{ Form::text('username') }}
+		</div>
+		<div class='text'>
+			{{ Form::label('Password') }}<br />
+			{{ Form::text('password') }}
+		</div>
+		<div class='checkbox'>
+			{{ Form::label('Remember me') }}
+			{{ Form::checkbox('remember') }}
+		</div>
+		{{ Form::token() }}
+		<div class='input-button'>
+			{{ Form::submit('Submit') }}
+		</div>
+		{{ Form::close() }}
+	</div>
+@stop
