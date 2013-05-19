0b93b46cf8945bd1cd0b8959f70673b8dae11f7e
Micah Bucy <micah.bucy@eternalsword.com>
2013-05-18 23:34:34 -0400

    add page wrapper to master layout

--- src/views/themes/default/layouts/master.blade.php
+++ src/views/themes/default/layouts/master.blade.php
@@ -20,10 +20,12 @@
 	<body>
 		<!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
+		<![endif]-->
 
-		<div id='content'>
-			@yield('content')
+		<div id='page'>
+			<div id='content'>
+				@yield('content')
+			</div>
 		</div>
 
 		@section('footer_scripts')
