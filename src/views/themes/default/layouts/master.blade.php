6449dfb8f0aa434bc6e65e41f975a6545ecf3391
Micah Bucy <micah.bucy@eternalsword.com>
2013-05-30 18:45:39 -0400

    last commit broke some stuff in ie7 & add ie7 to outdated

--- src/views/themes/default/layouts/master.blade.php
+++ src/views/themes/default/layouts/master.blade.php
@@ -23,7 +23,7 @@
 	</head>
 	<body>
 		<div id='page'>
-			<!--[if lt IE 7]>
+			<!--[if lt IE 8]>
 				<div class="message">
 					<p>You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 				</div>
