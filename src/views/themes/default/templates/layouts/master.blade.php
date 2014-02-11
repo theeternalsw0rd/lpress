b7011d7262dee6a586008f4c65dc63d46af95093
Micah Bucy <micah.bucy@eternalsword.com>
2014-02-11 14:14:58 -0500

    use variable instead of method call directly for easier scanning

--- src/views/themes/default/templates/layouts/master.blade.php
+++ src/views/themes/default/templates/layouts/master.blade.php
@@ -1,8 +1,9 @@
+{$ $ua_class = \EternalSword\LPress\UserAgent::getClass() $}
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-	<!--[if gt IE 8]><!--> <html lang="en" class="no-js{{ \EternalSword\LPress\UserAgent::getClass() }}"> <!--<![endif]-->
+	<!--[if gt IE 8]><!--> <html lang="en" class="no-js{{ $ua_class }}"> <!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
