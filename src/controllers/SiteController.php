896135345f787f47ee60ebda0fb5a4d657fbb07a
Micah Bucy <micah.bucy@eternalsword.com>
2014-03-12 14:08:05 -0400

    Site controller override base to check for root permissions

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -2,24 +2,19 @@
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\View;
 
 class SiteController extends BaseController {
-	public static function createSite() {
+	public static function getModelForm($slug, $model_name, $id = NULL) {
+		if(!Auth::user()->isRoot()) {
+			return App::abort(403, 'You do not have permission to modify this model.');
+		}
+		return parent::getModelForm($slug, $model_name, $id);
 	}
 
-	public static function updateSite($id) {
-	}
-
-	public static function getSites() {
-		extract(parent::prepareMake());
-		$sites = Site::paginate(15);
-		$pass_to_view = array(
-			'view_prefix' => $view_prefix,
-			'title' => 'Site Management',
-			'sites' => $sites,
-			'new_site' => new Site
-		);
-		return View::make($view_prefix . '.dashboard.sites.index', $pass_to_view);
+	public static function getModelIndex($slug, $model_name, $per_page) {
+		if(!Auth::user()->isRoot()) {
+			return App::abort(403, 'You do not have permission to modify this model.');
+		}
+		return parent::getModelIndex($slug, $model_name, $per_page);
 	}
 }
