c26b82c99cf67818e31bb3087cfc172e23274bc9
Micah Bucy <micah.bucy@eternalsword.com>
2014-04-04 17:45:10 -0400

    remove more cruft

--- src/views/themes/default/templates/dashboard/index.blade.php
+++ src/views/themes/default/templates/dashboard/index.blade.php
@@ -7,7 +7,7 @@
 	<!--<h2>Welcome, {{ $user->username }} {{-- HTML::linkRoute('lpress-logout', 'Logout') --}} </h2>-->
 	@if($is_root)
 		<p>{{ Lang::get('l-press::messages.dashboard_root_message') }}</p>
-		<h2>{{ Lang::get('l-press::headers.model_management', array('model' => 'Site')) }}</h2>
+		<h2>{{ Lang::get('l-press::headers.model_management', array('model' => 'Site')) }} {{ Form::new_model_link($new_site) }}</h2>
 		{{ HTML::collection_editor($sites, $new_site) }}
 		<a href="{{ URL::route('lpress-dashboard') }}/sites">{{ Lang::get('l-press::messages.manage_all', array('model' => 'sites')) }}</a>
 	@endif
