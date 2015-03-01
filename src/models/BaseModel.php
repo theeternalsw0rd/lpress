87382d25443524436d4e5f45af9a4f9edeb724ce
Micah Bucy <micah.bucy@eternalsword.com>
2015-03-01 12:09:51 -0500

    fix whitespace

--- src/models/BaseModel.php
+++ src/models/BaseModel.php
@@ -15,7 +15,6 @@ class BaseModel extends Eloquent {
 
 	protected $pivots = array();
 
-
 	protected function hasModelPermission($action) {
 		$user = Auth::user();
 		return $user->hasPermission('root');
