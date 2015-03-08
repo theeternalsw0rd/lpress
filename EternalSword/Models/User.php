9bba1b6a90a6ebe730dd6f6ab981b17e49aba519
Micah Bucy <micah.bucy@eternalsword.com>
2015-03-07 19:16:38 -0500

    fix use flag in user model

--- EternalSword/Models/User.php
+++ EternalSword/Models/User.php
@@ -1,6 +1,6 @@
 <?php namespace EternalSword\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
